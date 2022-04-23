4a506d3941aff74506fd4a51a075a1d312746ab9
Coding Mantras <mycodingmantras@gmail.com>
2022-04-23 22:26:22 +0530

    tmp

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-def get_series(n):
-    even_list = []
-    odd_list = []
-    square_list = []
-    for i in range(n):
-        if i % 2 == 0:
-            even_list.append(i)
-        elif i % 2 == 1:
-            odd_list.append(i)
-        square_list.append(i**2)
-
-    return even_list, odd_list, square_list
+from collections import Counter
 
 
-n = 10
-series = get_series(n)
+def get_series(sentence):
+    vowels = []
+    consonants = []
+    occurrences = Counter(sentence)
+    for i in sentence:
+        if i in ['a', 'e', 'i', 'o', 'u']:
+            vowels.append(i)
+        else:
+            consonants.append(i)
+
+    return vowels, consonants, occurrences
+
+
+sentence = "A quick brown fox jumps over the lazy dog"
+series = get_series(sentence)
 print(series[0])
 print(series[1])
 print(series[2])
